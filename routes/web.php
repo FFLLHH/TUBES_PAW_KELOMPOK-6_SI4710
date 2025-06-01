8f865e8cf3c63ae0b04eaa55cc7de1724c39aaee
FFLLHH <falah.adhicandra@gmail.com>
2025-06-01 23:56:35 +0700

    push routing dashboard dan produk

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,72 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Admin\ShopController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Client\ClientController;
+use App\Http\Controllers\Client\CartController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ShippingController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/login');
+
+Route::controller(ClientController::class)->group(function(){
+    Route::get('/home', 'products')->name('clientHome');
+    Route::get('/products', 'products')->name('clientProducts');
+    Route::get('/products-search', 'searchProduct')->name('clientProductSearch');
+    Route::get('/category', 'category')->name('clientCategory');
+    Route::get('/category/{category}', 'categoryProducts')->name('clientCategoryProducts');
+    Route::get('/product/{product}', 'productDetail')->name('clientProductDetail');
+    Route::get('/carts', 'carts')->name('clientCarts');
+    Route::post('/add-to-cart', 'addToCart')->name('clientAddToCart');
+    Route::post('/update-cart', 'updateCart')->name('clientUpdateCart');
+    Route::post('/delete-cart', 'deleteCart')->name('clientDeleteCart');
+    Route::get('/checkout', 'checkout')->name('clientCheckout');
+    Route::post('/checkout-save', 'checkoutSave')->name('clientCheckoutSave');
+    Route::get('/success/{order_code}', 'successOrder')->name('clientOrderCode');
+    Route::get('/check-order', 'checkOrder')->name('clientCheckOrder');
+    Route::post('/check-order-status', 'checkOrderStatus')->name('clientCheckOrderStatus');
+    Route::get('/about', 'about')->name('clientAbout');
+});
+
+Route::controller(CartController::class)->group(function(){
+    Route::get('/carts', 'carts')->name('clientCarts');
+    Route::post('/add-to-cart', 'addToCart')->name('clientAddToCart');
+    Route::post('/update-cart', 'updateCart')->name('clientUpdateCart');
+    Route::post('/delete-cart', 'deleteCart')->name('clientDeleteCart');
+});
+
+Auth::routes(['logout' => false]);
+Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [HomeController::class, 'index'])->name('home');
+
+    Route::controller(ShopController::class)->group(function() {
+        Route::post('/shop/create', 'create')->name('shopCreate');
+        Route::get('/shop', 'detail')->name('shopDetail');
+        Route::post('/shop/update', 'update')->name('shopUpdate');
+        Route::post('/shop/update-password', 'updatePassword')->name('shopUpdatePassword');
+    });
+
+    Route::controller(ProductController::class)->group(function() {
+        Route::get('/admin/products', 'index')->name('products');
+        Route::get('/admin/product/create', 'create')->name('productCreate');
+        Route::post('/admin/product/check', 'check')->name('productCheck');
+        Route::post('/admin/product/save', 'save')->name('producSave');
+        Route::post('/admin/product/images/', 'getImages')->name('productGetImages');
+        Route::get('/admin/product/images/{product}', 'addImages')->name('productAddImages');
+        Route::post('/admin/product/images/save', 'addImagesSave')->name('productAddImagesSave');
+        Route::post('/admin/product/images/delete', 'deleteImages')->name('productDeleteImages');
+        Route::get('/admin/product/edit/{product}', 'edit')->name('productEdit');
+        Route::post('/admin/product/edit/{product}/{id}/save', 'editSave')->name('productEditSave');
+        Route::get('/admin/product/delete/{id}', 'delete')->name('productDelete');
+    });
+
+    Route::resource('shippings', ShippingController::class);
 });
