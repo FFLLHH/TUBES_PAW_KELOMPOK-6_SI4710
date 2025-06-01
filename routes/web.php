8dc92cea06ba46b9f2b441ede3e061e4b00de2b8
Noviarhmadina <153992207+Noviarhmadina@users.noreply.github.com>
2025-06-01 17:36:05 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@ Route::controller(CategoryController::class)->group(function() {
     Route::get('/admin/category', 'index')->name('category');
     Route::get('/admin/category/create', 'create')->name('categoryCreate');
     Route::post('/admin/category/check', 'check')->name('categoryCheck');
-    
+    Route::post('/admin/category/save', 'save')->name('categorySave');
+    Route::get('/admin/category/delete/{id}/{path}', 'delete')->name('categoryDelete');
+    Route::get('/admin/category/edit/{id}', 'edit')->name('categoryEdit');
+    Route::post('/admin/category/update/{id}', 'update')->name('categoryUpdate');
 });
