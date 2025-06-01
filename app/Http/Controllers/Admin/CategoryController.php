f65d12de08c3c058782fcc3f63a5cecbe8076b9a
Noviarhmadina <153992207+Noviarhmadina@users.noreply.github.com>
2025-06-01 17:27:13 +0700

    fix controller

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -39,7 +39,30 @@ class CategoryController extends Controller
         }
     }
 
+    public function save(Request $request){
+        $validators = Validator::make($request->all(), [
+            'path' => 'required',
+            'name' => 'required|unique:categories',
+        ]);
 
+        if($validators->fails()){
+            return redirect()->route('categoryCreate')->withErrors($validators)->withInput();
+        }else{
+            $path = $request->file('path');
+            $extension_path = $path->getClientOriginalExtension();
+            $full_name_path = Str::random(20).".".$extension_path;
+            $path->move(public_path('shop/products/'), $full_name_path);
+
+            Category::create([
+                'shop_id' => Auth::user()->shop->id,
+                'name' => $request->name,
+                'path' => $full_name_path
+            ]);
+
+            return redirect()->route('category');
+
+        }
+    }
 
     public function delete($id, $path){
         $paths = public_path().'/shop/products/'. $path;
