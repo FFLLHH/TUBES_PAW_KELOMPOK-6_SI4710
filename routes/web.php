5fb80e9a5337755fb0e717eff3c52c796b34a591
RIZKY MUMTAZ <rizkyainun245@gmail.com>
2025-06-01 23:55:58 +0700

    izin ngepush routing manajemen user ketua

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+    Route::get('/users', [UserController::class, 'index'])->name('users');
+    Route::get('/users/create', [UserController::class, 'create'])->name('userCreate');
+    Route::post('/users/store', [UserController::class, 'store'])->name('userStore');
+    Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('userEdit');
+    Route::post('/users/update/{id}', [UserController::class, 'update'])->name('userUpdate');
+    Route::get('/users/delete/{id}', [UserController::class, 'destroy'])->name('userDelete');
