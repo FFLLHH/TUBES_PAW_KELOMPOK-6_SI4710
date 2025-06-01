2f382ebb7832c5c8812df3f3c08faf64b9cbe684
Helmi Afriza <helmi.afrz@gmail.com>
2025-06-01 23:40:04 +0700

    routes Orders

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,12 @@ Route::middleware(['auth', 'admin'])->group(function () {
     // Admin Dashboard
     Route::get('/admin', [HomeController::class, 'index'])->name('home');
 
-    
+    // Orders
+    Route::controller(OrderController::class)->group(function() {
+        Route::get('/admin/orders', 'index')->name('orders');
+        Route::get('/admin/order/{order_code}', 'detail')->name('orderDetail');
+        Route::post('/admin/order/update-status/{order_code}', 'updateStatus')->name('orderUpdateStatus');
+        Route::get('/admin/order/delete/{order_code}', 'delete')->name('orderDelete');
+    });
+
 });
