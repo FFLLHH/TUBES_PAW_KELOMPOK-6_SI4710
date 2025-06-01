5b7168209828365922eb06607f215f93b142ea3e
Noviarhmadina <153992207+Noviarhmadina@users.noreply.github.com>
2025-06-01 17:32:19 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Category
+Route::controller(CategoryController::class)->group(function() {
+    Route::get('/admin/category', 'index')->name('category');
+    Route::get('/admin/category/create', 'create')->name('categoryCreate');
+    Route::post('/admin/category/check', 'check')->name('categoryCheck');
+    
 });
